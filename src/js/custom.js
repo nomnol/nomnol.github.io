caa636a60ef7a9a1e5e153a433551b9c85049180
nomnol <nomnol@tais.ru>
2018-03-22 10:59:02 +0300

    hmr don't work, start grid

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,7 +1,1 @@
-const f = function (str) {
-  return function (arg) {
-    return [str, [str, str], arg];
-  };
-};
-
-f('Сейчас будет ошибка')()[1, 2].forEach(alert)
+console.log('custom.js is running');
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import HotModuleReplacementPlugin from 'webpack/lib/HotModuleReplacementPlugin.js';
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/js/index.js'),
@@ -15,9 +16,14 @@ module.exports = {
         use: 'babel-loader'
       },
       {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader'
+        })
+      },
+      {
         test: /\.cssnext$/,
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
           use: [
             {
               loader: 'css-loader?sourceMap'
@@ -29,6 +35,7 @@ module.exports = {
                 sourceMap: true,
                 plugins: (loader) => [
                   require('postcss-import')({ root: loader.resourcePath }),
+                  require('postcss-apply')(),
                   require('postcss-nested')(),
                   require('postcss-url')(),
                   require('postcss-cssnext')()
@@ -40,9 +47,13 @@ module.exports = {
       },
     ]
   },
-  plugins: [new ExtractTextPlugin('css/index.css')],
+  plugins: [
+    new ExtractTextPlugin('css/index.css'),
+    new HotModuleReplacementPlugin()
+  ],
   devServer: {
-    port: 9000,
+    port: 9010,
+    hot: true,
     open: true
   },
   devtool: 'source-map'
